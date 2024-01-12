6569a7d2d9168f995092e238cea3af5a5e5eaa3f
Aaditya Nair <128372678+FreshPrince99@users.noreply.github.com>
2024-01-12 10:29:38 +0000

    Update LEDPanel.c

--- LEDPanel.c
+++ LEDPanel.c
@@ -3,25 +3,18 @@
 #include "libopencm3/stm32/adc.h" //Needed to convert analogue signals to digital
 
 #define IOPORT GPIOA
-#define JOYSTICK_A_PORT GPIOA
-#define JOYSTICK_B_PORT GPIOC
 #define ADC_REG ADC1
-#define BUTTON GPIO4 // --> is this required?? buttons are usually only for digital inputs we are only dealing with analog inputs such as the joysticks
+
 // Define GPIO pins
-#define GPIO_LED    GPIOC
-#define GPIO_CLOCK  GPIOC
-#define GPIO_LATCH  GPIOC
-#define GPIO_ROWS   GPIOC
-#define INPUT_PIN  GPIO6
+#define GPIO_LED    GPIOC // for the input pin
+#define GPIO_CLOCK  GPIOC // for the clock
+#define GPIO_LATCH  GPIOC // for the latch
+#define GPIO_ROWS   GPIOC // for the rows
+#define INPUT_PIN  GPIO6 
 #define CLOCK_PIN  GPIO7
 #define LATCH_PIN  GPIO8
-#define ROWS_MASK  (GPIO2 | GPIO3 | GPIO4 | GPIO5)
 
-// Constants defining the relevant control bits (D12-D15) that are appended to
-// both the X and Y-coordinates to facilitate an appropriate transmission to the DAC
-#define portA 0xc000
-#define portB 0x4000
-
+// Game interface variables
 #define PANEL_WIDTH  16
 #define PANEL_HEIGHT 32
 #define symbolSegmentLength 5
@@ -30,8 +23,8 @@
 #define scoreBeforeReset (winningScore + 3)
 #define batWidth 2
 #define batSpacing 2
-#define maxPaddleValue 105
-#define minPaddleValue 555  // both have to be tested
+#define maxPaddleVal 105
+#define minPaddleVal 555  // both have to be tested
 #define batLength 4
 #define ballRadius 2
 #define winningScore 5
@@ -44,90 +37,11 @@ int ballSpeed;                          // Speed of the ball
 int winner;                             // Flag indicating the winner of the match
 int leftBatOffset, rightBatOffset;      // Vertical displacement of the bat from the bottom of the zone
 int ballX, ballY;                       // The coordinates of the ball
-int ballControlX = 1, ballControlY = 1; // Velocity control variables for determining the balls direction
-int leftScore, rightScore;              // Respective scores of players 1 and 2
+int ballVelocityX = 1, ballVelocityY = 1; // Velocity control variables for determining the balls direction
+int leftScore, rightScore;              // Respective scores for both the players
 uint32_t leftTop, leftDown, rightTop, rightDown; //Directions for both the paddles
 
-// Function to perform a clock pulse for each binary digit (Mentioned on the coursework website)
-void performClockPulses(uint32_t data) {
-    rcc_periph_clock_enable(RCC_GPIOC);
-    rcc_periph_clock_enable(RCC_GPIOD);
-
-    // Configure GPIO pins
-    setup();
-
-    // Iterate through each bit
-    for (int i = 0; i < 32; i++) {
-        // Set the input pin to the current bit value
-        if ((data >> i) & 1) {
-            gpio_set(GPIOC, INPUT_PIN);
-        } else {
-            gpio_clear(GPIOC, INPUT_PIN);
-        }
-
-        // Iterate through each row selection pin
-        for (int row = 0; row < 32; row++) {
-            // Set the current row selection pin
-            if (row == i % 32) {
-                gpio_set(GPIOD, ROWS_MASK);
-            } else {
-                gpio_clear(GPIOD, ROWS_MASK);
-            }
-        }
-
-        // Simulate a clock pulse by toggling the clock pin
-        gpio_set(GPIOC, CLOCK_PIN);
-        gpio_clear(GPIOC, CLOCK_PIN);
-
-        // Simulate a latch pulse 
-        gpio_set(GPIOC, LATCH_PIN);
-        gpio_clear(GPIOC, LATCH_PIN);
-
-        // Simulate a delay
-        for (volatile int j = 0; j < 100000; j++) {}
-    }
-}
-// Function to set a specific row and call performClockPulses (Mentioned on the coursework website)
-void setRowAndPerformClockPulses(uint32_t data, int row) {
-    rcc_periph_clock_enable(RCC_GPIOC);
-    rcc_periph_clock_enable(RCC_GPIOD);
-
-    // Configure GPIO pins
-    setup();
-
-    // Set the desired row
-    for (int r = 0; r < 32; r++) {
-        if (r == row) {
-            gpio_set(GPIOD, ROWS_MASK);
-        } else {
-            gpio_clear(GPIOD, ROWS_MASK);
-        }
-    }
-
-    // Call the function with the 32-bit integer
-    performClockPulses(data);
-}
-// Function to update the display with red, green, and blue data for each half (Mentioned in on the coursework website)
-void updateDisplay(uint32_t redData1, uint32_t greenData1, uint32_t blueData1,
-                   uint32_t redData2, uint32_t greenData2, uint32_t blueData2,
-                   int row) {
-    // Latch before updating
-    gpio_set(GPIOC, LATCH_PIN);  // Assuming GPIOC_PIN_8 is the latch pin
-    gpio_clear(GPIOC, LATCH_PIN);
-
-    // Update display for the first half
-    setRowAndPerformClockPulses(redData1, row);
-    setRowAndPerformClockPulses(greenData1, row);
-    setRowAndPerformClockPulses(blueData1, row);
-
-    // Update display for the second half
-    setRowAndPerformClockPulses(redData2, row);
-    setRowAndPerformClockPulses(greenData2, row);
-    setRowAndPerformClockPulses(blueData2, row);
-
-    // No need to latch again as it's done once before updating
-}
-
+//This function draws a vertical line of length l downwards from (x, y)
 void drawVerticalLine(int x, int y, int l)
 {
   int i;
@@ -153,13 +67,13 @@ void drawPoint(int x, int y) {
         uint16_t pin = 1 << (x % 16);
 
         // Set the corresponding pin in the GPIO register for the specified row
-        gpio_set(GPIOC, LATCH_PIN);  // Set latch
-        gpio_clear(GPIOC, ROWS_MASK); // Clear all rows
-        gpio_set(GPIOC, (ROWS_MASK & ~(1 << (y + 2)))); // Set the specific row
-        gpio_clear(GPIOC, LATCH_PIN);  // Clear latch
+        gpio_set(GPIO_LATCH, LATCH_PIN);  // Set latch
+        gpio_clear(GPIOC, ROW_PINS); // Clear all rows
+        gpio_set(GPIOC, (ROW_PINS & ~(1 << (y + 2)))); // Set the specific row
+        gpio_clear(GPIO_LATCH, LATCH_PIN);  // Clear latch
 
         // Set the pin corresponding to the x-coordinate
-        gpio_set(GPIOC, pin);
+        gpio_set(GPIO_LED, pin);
     }
 }
 
@@ -217,7 +131,7 @@ void drawBall(){
 }
 
 // This function is responsible for rendering the bats
-void drawBats(){ // Needs correction
+void drawBats(){ 
   drawRect(PANEL_WIDTH - batSpacing - batWidth, rightBatOffset, batWidth, batLength);
   drawRect(batSpacing, leftBatOffset, batWidth, batLength);
 }
@@ -237,7 +151,7 @@ void drawScores(){
   drawDigit(PANEL_WIDTH/2 + symbolSpacing*2 + symbolSegmentLength, PANEL_HEIGHT - symbolSegmentLength*2 - scoresOffset, rightScore%10);
 }
 
-
+//Draws a digit to display the player number where n is the number
 void drawDigit(int x, int y, int n){
   // Ensure that n is within the valid range by assusinmg its unit value
   n %= 10;
@@ -289,21 +203,47 @@ void batCollision(){
   // If the ball strikes a bat, rebound it in the opposite direction and increase the ball's speed
   if(
     // Check the left bat
-    (ballControlX < 0 && ballX - ballRadius < batWidth + batSpacing && ballY > leftBatOffset && ballY < leftBatOffset + batLength) ||
+    (ballVelocityX < 0 && ballX - ballRadius < batWidth + batSpacing && ballY > leftBatOffset && ballY < leftBatOffset + batLength) ||
     // Check the right bat
-    (ballControlX > 0 && ballX + ballRadius > PANEL_WIDTH - batWidth - batSpacing && ballY > rightBatOffset && ballY < rightBatOffset + batLength)
+    (ballVelocityX > 0 && ballX + ballRadius > PANEL_WIDTH - batWidth - batSpacing && ballY > rightBatOffset && ballY < rightBatOffset + batLength)
   )
   {
-    ballControlX = -ballControlX;
+    ballVelocityX = -ballVelocityX;
     increaseSpeed();
   }
 }
+void moveBall() { // updates the co-ordinates of the ball
+  ballX += ballVelocityX * ballSpeed;
+  ballY += ballVelocityY * ballSpeed;
+}
 
-void increaseSpeed() {
+void increaseSpeed() { // increases the speed of the ball
     ballSpeed += speedIncrement;
     if (ballSpeed > maxSpeed){
         ballSpeed = maxSpeed;
     }
+}
+void zoneCollision(){
+  // Check if the ball exits the game zone by moving right
+  if(ballX + ballRadius > PANEL_WIDTH)
+  {
+    ballVelocityX = -ballVelocityX;
+    leftScore++;
+    resetBall();
+  }
+  // Check if the ball exits the game zone by moving left
+  else if(ballX - ballRadius < 0)
+  {
+    ballVelocityX = -ballVelocityX;
+    rightScore++;
+    resetBall();
+  }
+
+  // Check if the ball exits the game zone by moving either up or down
+  if(ballY + ballRadius > PANEL_HEIGHT || ballY - ballRadius < 0)
+  {
+    ballVelocityY = -ballVelocityY;
+  }
 }
 
 void pulse(int pin){ //Performs input and a clock pulse
@@ -313,19 +253,19 @@ void pulse(int pin){ //Performs input and a clock pulse
     return 0;
 }
 
-void set(int pin){
-  gpio_clear(GPIOC, CLOCK_PIN);
-  gpio_set(GPIOC, pin);
-  gpio_set(GPIOC, CLOCK_PIN);
+void set(int pin){ // sets the input pin
+  gpio_clear(GPIO_CLOCK, CLOCK_PIN);
+  gpio_set(GPIO_LED, pin);
+  gpio_set(GPIO_CLOCK, CLOCK_PIN);
 }
 
-void clear(int pin){
-  gpio_clear(GPIOC, CLOCK_PIN);
-  gpio_clear(GPIOC, pin);
-  gpio_set(GPIOC, CLOCK_PIN);
+void clear(int pin){ // clears the input pin
+  gpio_clear(GPIO_CLOCK, CLOCK_PIN);
+  gpio_clear(GPIO_LED, pin);
+  gpio_set(GPIO_CLOCK, CLOCK_PIN);
 }
 
-void clearAllRows(void){
+void clearAllRows(void){ // clears all the row pins
     gpio_clear(GPIOC, GPIO2);
     gpio_clear(GPIOC, GPIO3);
     gpio_clear(GPIOC, GPIO4);
@@ -333,20 +273,20 @@ void clearAllRows(void){
 }
 
 
-void clock(void){
-  gpio_clear(GPIOC, CLOCK_PIN);
-  gpio_set(GPIOC, CLOCK_PIN);
+void clock(void){ // sets the clock 
+  gpio_clear(GPIO_CLOCK, CLOCK_PIN);
+  gpio_set(GPIO_CLOCK, CLOCK_PIN);
 }
 
 
-void setup(void){
+void setup(void){ //sets all the GPIO pins
   rcc_periph_clock_enable(RCC_GPIOC);
-  gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LATCH_PIN);            //GPIO Port Name, GPIO Mode, GPIO Push Up Pull Down Mode, GPIO Pin Number
-  gpio_set_output_options(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, LATCH_PIN); 
-  gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, CLOCK_PIN);            //GPIO Port Name, GPIO Mode, GPIO Push Up Pull Down Mode, GPIO Pin Number
-  gpio_set_output_options(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, CLOCK_PIN);
-  gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, INPUT_PIN);            //GPIO Port Name, GPIO Mode, GPIO Push Up Pull Down Mode, GPIO Pin Number
-  gpio_set_output_options(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, INPUT_PIN);
+  gpio_mode_setup(GPIO_LATCH, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LATCH_PIN);            //GPIO Port Name, GPIO Mode, GPIO Push Up Pull Down Mode, GPIO Pin Number
+  gpio_set_output_options(GPIO_LATCH, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, LATCH_PIN); 
+  gpio_mode_setup(GPIO_CLOCK, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, CLOCK_PIN);            //GPIO Port Name, GPIO Mode, GPIO Push Up Pull Down Mode, GPIO Pin Number
+  gpio_set_output_options(GPIO_CLOCK, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, CLOCK_PIN);
+  gpio_mode_setup(GPIO_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, INPUT_PIN);            //GPIO Port Name, GPIO Mode, GPIO Push Up Pull Down Mode, GPIO Pin Number
+  gpio_set_output_options(GPIO_LED, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, INPUT_PIN);
   gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);            //GPIO Port Name, GPIO Mode, GPIO Push Up Pull Down Mode, GPIO Pin Number
   gpio_set_output_options(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, GPIO5);
   gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO4);            //GPIO Port Name, GPIO Mode, GPIO Push Up Pull Down Mode, GPIO Pin Number
@@ -371,10 +311,9 @@ void configRegisters() {
     adc_set_resolution(ADC_REG, ADC_CFGR1_RES_12_BIT);  //Get a good resolution
 
     adc_power_on(ADC_REG);  //Finished setup, turn on ADC register 1
-
-    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, INPUT_PIN | CLOCK_PIN | LATCH_PIN | ROWS_MASK);
-}
-uint32_t channelSetup(int val){
+    setup();
+  }
+uint32_t channelSetup(int val){ //For setting up channels for each direction
   uint8_t channelArray = {val};  //Define a channel that we want to look at
   adc_set_regular_sequence(ADC_REG, 1, channelArray);  //Set up the channel
   adc_start_conversion_regular(ADC_REG);  //Start converting the analogue signal
@@ -403,7 +342,6 @@ int main(void) {
   clearAllRows();
   clear();
   set();
-  setup();
 
   while (1){
     
@@ -422,32 +360,30 @@ input(){
   leftDown = channelSetup(2);
   rightTop = channelSetup(6);
   rightDown = channelSetup(7);
-  
-  // *ADC_REG = 0x2;
-  // while (*ADC_REG & 0x10 != 1);
-  if(leftTop != 0){
-    uint32_t temp = leftTop;
-  }
-  else{
-    uint32_t temp = leftDown;
-  }
-  int leftPaddleValue = (int)temp;
-  leftBatOffset = (leftPaddleValue - minPaddleValue)
-                  *(PANEL_HEIGHT - batLength)
-                  /(maxPaddleValue - minPaddleValue);
 
-  // *ADC_REG = 0x2;
-  // while(*ADC_REG & 0x20 != 1);
-  if (rightTop!= 0){
-    int temp = rightTop ;
+  uint32_t temp;
+  if(leftTop != 0){
+    temp = leftTop;
   }
   else{
-    int temp = rightDown;
+    temp = leftDown;
   }
-  int rightPaddleValue = temp; //does'nt seem like the value is being receieved after the if and else stmns??
-  rightBatOffset = (rightPaddleValue - minPaddleValue)
+  int leftPaddleVal = (int)temp;
+  leftBatOffset = (leftPaddleVal - minPaddleVal)
+                  *(PANEL_HEIGHT - batLength)
+                  /(maxPaddleVal - minPaddleVal);
+
+  uint32_t temp;
+  if (rightTop!= 0){
+    temp = rightTop ;
+  }
+  else{
+    temp = rightDown;
+  }
+  int rightPaddleValue = (int)temp; //does'nt seem like the value is being receieved after the if and else stmns??
+  rightBatOffset = (rightPaddleValue - minPaddleVal)
                    *(PANEL_HEIGHT - batLength)
-                   /(maxPaddleValue - minPaddleValue);
+                   /(maxPaddleVal - minPaddleVal);
 
 }
 
@@ -458,7 +394,7 @@ update(){
 
   if(leftScore >= winningScore){
     winner = 1; // Indicate that player 1 has won the match
-    ballControlX = 1; // Specify ball Velocity to favour the winner
+    ballVelocityX = 1; // Specify ball Velocity to favour the winner
 
     if(leftScore >= scoreBeforeReset)
     {
@@ -467,7 +403,7 @@ update(){
   }
   else if(rightScore >= winningScore){
     winner = 2;
-    ballControlX = -1;   
+    ballVelocityX = -1;   
 
     if(rightScore >= scoreBeforeReset)
     {
